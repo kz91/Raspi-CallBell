6bfa37c36316e8664acc07019caf2f06f93cf968
kz91 <kznyan91@gmail.com>
2024-03-02 21:06:17 +0900

    微調整

--- pico-script.py
+++ pico-script.py
@@ -87,6 +87,7 @@ class RequestHandler:
 
 
 def main():
+    ################
     SSID = 'Wifi SSID'
     PW = 'Wifi Password'
     IP_ADDRESS = 'IP Address'
@@ -95,6 +96,8 @@ def main():
     POWER_PIN = 17
     LED_AND_BZ_PIN = 18
     SW_PIN = 22
+    POWER_PIN = 36
+    ################
 
     wlan_manager = WLANManager(SSID, PW, IP_ADDRESS)
     http_request_wait = HTTPRequestWait(IP_ADDRESS, PORT)
